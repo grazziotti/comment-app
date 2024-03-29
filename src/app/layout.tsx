06a547788eaf2789d11aef5695e0d6929a22d353
Daniel <dgplays14@gmail.com>
2024-03-29 01:22:00 -0300

    style: update Rubik font configuration in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,14 @@ import type { Metadata } from 'next'
 import { Rubik } from 'next/font/google'
 
 import './globals.css'
+
 import ReactQueryProvider from '@/providers/ReactQueryProvider'
 
-const rubik = Rubik({ subsets: ['latin'] })
+const rubik = Rubik({
+  subsets: ['latin'],
+  preload: true,
+  weight: ['400', '500', '700']
+})
 
 export const metadata: Metadata = {
   title: 'Interactive comments section',
