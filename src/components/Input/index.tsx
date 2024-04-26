d911449decd69d497ef90011ea82847622642c9a
Daniel <dgplays14@gmail.com>
2024-04-26 18:17:23 -0300

    feat(components/LoginForm): implement error handling

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,3 +1,9 @@
+'use client'
+
+import { useState } from 'react'
+
+import { Eye, EyeOff } from 'lucide-react'
+
 type InputProps = {
   id: string
   type: 'text' | 'password'
@@ -5,6 +11,9 @@ type InputProps = {
   placeholder: string
   required?: boolean
   label?: string
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  value?: string
+  isError?: boolean
 }
 
 export default function Input({
@@ -13,23 +22,47 @@ export default function Input({
   placeholder,
   type,
   label,
-  required
+  required,
+  onChange,
+  value,
+  isError
 }: InputProps) {
+  const [showPassword, setShowPassword] = useState(false)
+
+  function togglePasswordVisibility(e: React.FormEvent<HTMLButtonElement>) {
+    e.preventDefault()
+    setShowPassword((prevShowPassword) => !prevShowPassword)
+  }
+
   return (
     <>
       {label && (
         <label htmlFor={id} className="text-textTitle">
           {label}
+          {required && <span className="text-delete">*</span>}
         </label>
       )}
-      <input
-        className="mb-4 mt-1 min-w-72 rounded-xl border-2 border-gray-200 px-4 py-2 text-textBody outline-none transition-colors hover:border-target focus:border-target"
-        id={id}
-        type={type}
-        name={name}
-        required={required}
-        placeholder={placeholder}
-      />
+      <div className="relative flex items-center">
+        <input
+          className={`mb-1 mt-1 ${isError && 'border-delete'} flex w-full items-center rounded-xl border-2 border-gray-200 px-4 py-2 pr-10 text-textBody outline-none transition-colors hover:border-target focus:border-target`}
+          id={id}
+          type={type === 'text' ? 'text' : showPassword ? 'text' : 'password'}
+          name={name}
+          required={required}
+          placeholder={placeholder}
+          onChange={onChange}
+          value={value}
+        />
+        {type === 'password' && (
+          <button
+            className="absolute right-[4%] cursor-default text-textTitle"
+            type="button"
+            onClick={togglePasswordVisibility}
+          >
+            {showPassword ? <Eye size={22} /> : <EyeOff size={22} />}
+          </button>
+        )}
+      </div>
     </>
   )
 }
--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 import Fieldset from '../Fieldset'
 import Input from '../Input'
@@ -9,6 +11,53 @@ import { checkPassword } from '@/utils/checkPassword'
 import { hasLetter } from '@/utils/hasLetter'
 
 export default function LoginForm() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [usernameError, setUsernameError] = useState<boolean>(false)
+  const [passwordError, setPasswordError] = useState<boolean>(false)
+  const [loginError, setLoginError] = useState<boolean>(false)
+  const [ableLoginButton, setAbleLoginButton] = useState<boolean>(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    checkLoginButtonState()
+  }, [username, password, loginError])
+
+  function checkLoginButtonState() {
+    if (username.length < 1 || password.length < 1 || loginError) {
+      setAbleLoginButton(false)
+      return
+    }
+
+    setAbleLoginButton(true)
+  }
+
+  function handleChangePassword(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value
+    setPassword(value)
+    setLoginError(false)
+
+    if (value.length < 1) {
+      setPasswordError(true)
+      return
+    }
+
+    setPasswordError(false)
+  }
+
+  function handleChangeUsername(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value
+    setUsername(value)
+    setLoginError(false)
+
+    if (value.length < 1) {
+      setUsernameError(true)
+      return
+    }
+
+    setUsernameError(false)
+  }
+
   async function login(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
     const formData = new FormData(e.currentTarget)
@@ -17,12 +66,12 @@ export default function LoginForm() {
     const password = formData.get('password') as string
 
     if (username.length <= 1) {
-      console.log('error: username must have at least 2 characters')
+      setLoginError(true)
       return
     }
 
     if (!hasLetter(username)) {
-      console.log('error: username must contain letters')
+      setLoginError(true)
       return
     }
 
@@ -32,45 +81,77 @@ export default function LoginForm() {
     }
 
     if (!checkPassword(password)) {
-      console.log(
-        'error: password must have at least 8 characters, one uppercase letter, one lowercase letter, one number, one special character'
-      )
+      setLoginError(true)
       return
     }
 
-    console.log('success!')
-
-    await signIn('credentials', {
+    const loginResult = await signIn('credentials', {
       ...data,
-      redirect: true
+      redirect: false
     })
+
+    if (loginResult?.status === 200) {
+      router.push('/')
+      router.refresh()
+      return
+    }
+
+    setUsernameError(false)
+    setPasswordError(false)
+    setLoginError(true)
   }
 
   return (
-    <form className="mt-6" onSubmit={login}>
+    <form className="mt-6 max-w-72" onSubmit={login}>
       <Fieldset>
-        <Input
-          id="username"
-          type="text"
-          name="username"
-          required
-          label="Username"
-          placeholder="Enter Username"
-        />
+        <div className="mb-4">
+          <Input
+            id="username"
+            name="username"
+            placeholder="Enter username"
+            type="text"
+            label="Username"
+            onChange={handleChangeUsername}
+            required
+            value={username}
+            isError={usernameError || loginError}
+          />
+          {usernameError && (
+            <p className="break-all text-sm text-red-400">
+              This field is required.
+            </p>
+          )}
+        </div>
       </Fieldset>
       <Fieldset>
-        <Input
-          id="password"
-          name="password"
-          type="password"
-          required
-          label="Password"
-          placeholder="Enter Password"
-        />
+        <div className="mb-4">
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            required
+            label="Password"
+            placeholder="Enter Password"
+            isError={passwordError || loginError}
+            onChange={handleChangePassword}
+            value={password}
+          />
+          {passwordError && (
+            <p className="break-all text-sm text-red-400">
+              This field is required.
+            </p>
+          )}
+          {loginError && (
+            <p className="break-all text-sm text-red-400">
+              Invalid username or password.
+            </p>
+          )}
+        </div>
       </Fieldset>
       <button
         type="submit"
-        className="mt-4 w-full rounded-lg bg-target py-2 font-medium text-primary transition-colors hover:bg-targetInactive"
+        className={`mt-4 w-full ${!ableLoginButton && 'bg-targetInactive'} rounded-lg bg-target py-2 font-medium text-primary transition-colors hover:bg-targetInactive`}
+        disabled={!ableLoginButton}
       >
         Login
       </button>
