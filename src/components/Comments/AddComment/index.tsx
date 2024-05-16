82301fc81a6bbf38a60bf96dc15cbbecc4a118f7
Daniel <dgplays14@gmail.com>
2024-05-16 03:42:06 -0300

    feat(components): implement add reply

--- src/components/Comments/AddComment/index.tsx
+++ src/components/Comments/AddComment/index.tsx
@@ -3,43 +3,49 @@ import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
 import { useAddComment } from '@/hooks/useAddComment'
+import { useAddCommentReply } from '@/hooks/useAddCommentReply'
 import { useQueryClient } from '@tanstack/react-query'
 
-export default function PrivateAddComment() {
+type Props = {
+  replyToId?: string
+  replyTo?: string
+  onDone?: () => void
+}
+
+export default function AddComment({ replyToId, replyTo, onDone }: Props) {
   const { data: session } = useSession()
   const router = useRouter()
   const queryClient = useQueryClient()
-  const [isSendButtonEnabled, setIsSendButtonEnabled] = useState(false)
 
-  const { mutate, isSuccess } = useAddComment()
+  const { mutate: addComment, isSuccess: commentAddSuccess } = useAddComment()
+  const { mutate: addCommentReply, isSuccess: replyAddSuccess } =
+    useAddCommentReply()
   const [comment, setComment] = useState('')
+  const [isCommentAllowed, setIsCommentAllowed] = useState<boolean>()
 
   useEffect(() => {
-    setIsSendButtonEnabled(comment.length > 0)
+    if (replyToId && replyTo) {
+      setComment(`@${replyTo}, `)
+    }
+  }, [])
+
+  useEffect(() => {
+    checkCommentAllowed()
   }, [comment])
 
   useEffect(() => {
-    queryClient.invalidateQueries({ queryKey: ['comment-data'] })
-  }, [isSuccess])
+    if (commentAddSuccess || replyAddSuccess) {
+      if (onDone) {
+        onDone()
+      }
+
+      queryClient.invalidateQueries({ queryKey: ['comment-data'] })
+    }
+  }, [commentAddSuccess, replyAddSuccess])
 
   function handleChangeComment(e: React.ChangeEvent<HTMLTextAreaElement>) {
     const value = e.target.value
     setComment(value)
-  }
-
-  function handleSendButtonClick() {
-    if (!session) {
-      router.push('/login')
-      return
-    }
-
-    if (comment.length < 1) {
-      return
-    }
-
-    mutate({ content: comment, token: session.user.token })
-
-    setComment('')
   }
 
   function handleTextareaFocus() {
@@ -48,14 +54,73 @@ export default function PrivateAddComment() {
     }
   }
 
+  function handleSendButtonClick() {
+    if (!session) {
+      router.push('/login')
+      return
+    }
+
+    if (!isCommentAllowed) return
+
+    const token = session.user.token
+
+    if (replyToId && replyTo) {
+      const replyData = {
+        replyToId: replyToId,
+        content: filterReply(comment.trim(), replyTo)
+      }
+
+      addCommentReply({ data: replyData, token })
+      return
+    } else {
+      addComment({ content: comment.trim(), token: session.user.token })
+    }
+
+    setComment('')
+  }
+
+  function checkCommentAllowed() {
+    if (!replyToId) {
+      setIsCommentAllowed(!isEmptyOrSpaces(comment.trim()))
+      return
+    } else {
+      if (replyTo) {
+        const filteredReply = filterReply(comment.trim(), replyTo)
+        setIsCommentAllowed(!isEmptyOrSpaces(filteredReply.trim()))
+        return
+      }
+
+      return
+    }
+  }
+
+  const isEmptyOrSpaces = (str: string) => {
+    return str.trim() === null || str.trim().match(/^ *$/) !== null
+  }
+
+  const filterReply = (comment: string, mention: string) => {
+    const regex = new RegExp(`@${mention}\\b`, 'i')
+
+    if (regex.test(comment)) {
+      const filteredComment = comment.substring(
+        comment.indexOf(`@${mention},`) + `@${mention},`.length
+      )
+
+      return filteredComment
+    }
+
+    return comment
+  }
+
   return (
     <div className="my-6 flex gap-x-6 bg-primary p-6">
       <div>
         <div className="h-8 w-8 rounded-full bg-slate-200"></div>
       </div>
       <textarea
+        autoFocus={replyToId ? true : false}
         className="flex h-24 w-full items-center rounded-xl border-2 px-6 py-3 pr-10 text-textBody outline-none transition-colors hover:border-target focus:border-target"
-        placeholder="Add a comment..."
+        placeholder={`Add a ${replyToId && replyTo ? 'reply' : 'comment'}...`}
         value={comment}
         onChange={handleChangeComment}
         onFocus={handleTextareaFocus}
@@ -63,10 +128,10 @@ export default function PrivateAddComment() {
       <div className="w-full max-w-24">
         <button
           onClick={handleSendButtonClick}
-          className={`w-full ${isSendButtonEnabled ? 'bg-target' : 'bg-targetInactive'} rounded-xl py-3 font-bold text-primary transition-colors hover:bg-targetInactive`}
-          disabled={!isSendButtonEnabled}
+          className={`w-full ${isCommentAllowed ? 'bg-target' : 'bg-targetInactive'} rounded-xl py-3 font-bold text-primary transition-colors hover:bg-targetInactive`}
+          disabled={!isCommentAllowed}
         >
-          SEND
+          {replyToId ? 'REPLY' : 'SEND'}
         </button>
       </div>
     </div>
--- src/components/Comments/Comment/index.tsx
+++ src/components/Comments/Comment/index.tsx
@@ -1,7 +1,14 @@
+import { useSession } from 'next-auth/react'
+import { redirect } from 'next/navigation'
+import { useState } from 'react'
+
+import AddComment from '../AddComment'
+
 import { formatRelativeTime } from '@/utils/formatRelativeTime'
 import { Minus, Plus, Reply } from 'lucide-react'
 
 type Props = {
+  commentId: string
   content: string
   username: string
   createdAt: Date
@@ -10,53 +17,80 @@ type Props = {
 }
 
 export default function Comment({
+  commentId,
   content,
   username,
   createdAt,
   score,
   replyTo
 }: Props) {
+  const { data: session } = useSession()
+
   const currentTime = new Date()
   const createdAtDate = new Date(createdAt)
   const timeDifference = currentTime.getTime() - createdAtDate.getTime()
   const formattedTimeDifference = formatRelativeTime(timeDifference)
 
+  const [showAddCommentReply, setShowAddReplyComment] = useState(false)
+
+  function handleReplyBtnClick() {
+    if (!session) {
+      redirect('/login')
+    }
+
+    if (session.user.name === username) return
+
+    setShowAddReplyComment(!showAddCommentReply)
+  }
+
   return (
-    <div className="mt-6 flex w-full rounded-xl bg-white p-6">
-      <div className="inline-flex flex-col items-center gap-y-3 rounded-xl bg-secondary px-3 py-2">
-        <button className="flex h-5 w-5 items-center justify-center text-targetInactive transition-colors hover:text-target">
-          <Plus size={15} strokeWidth={4} />
-        </button>
-        <span tabIndex={0} className="font-bold text-target">
-          {score}
-        </span>
-        <button className="flex h-5 w-5 items-center justify-center text-targetInactive transition-colors hover:text-target">
-          <Minus size={15} strokeWidth={4} />
-        </button>
-      </div>
-      <div className="ml-6 flex w-full flex-col gap-y-3">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center">
-            <div className="h-8 w-8 rounded-full bg-secondary"></div>
-            <span tabIndex={0} className="ml-3 font-medium text-textTitle">
-              {username}
-            </span>
-            <span tabIndex={0} className="ml-4 text-textBody">
-              {formattedTimeDifference} ago
-            </span>
-          </div>
-          <button className="flex items-start gap-x-1 font-bold text-target transition-colors hover:text-targetInactive">
-            <Reply size={18} strokeWidth={4} />
-            Reply
+    <div>
+      <div className="mt-6 flex w-full rounded-xl bg-white p-6">
+        <div className="inline-flex flex-col items-center gap-y-3 rounded-xl bg-secondary px-3 py-2">
+          <button className="flex h-5 w-5 items-center justify-center text-targetInactive transition-colors hover:text-target">
+            <Plus size={15} strokeWidth={4} />
+          </button>
+          <span tabIndex={0} className="font-bold text-target">
+            {score}
+          </span>
+          <button className="flex h-5 w-5 items-center justify-center text-targetInactive transition-colors hover:text-target">
+            <Minus size={15} strokeWidth={4} />
           </button>
         </div>
-        <p tabIndex={0} className="break-words leading-normal text-textBody">
-          {replyTo && (
-            <span className="mr-1 font-medium text-target">@{replyTo}</span>
-          )}
-          {content}
-        </p>
+        <div className="ml-6 flex w-full flex-col gap-y-3">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <div className="h-8 w-8 rounded-full bg-secondary"></div>
+              <span tabIndex={0} className="ml-3 font-medium text-textTitle">
+                {username}
+              </span>
+              <span tabIndex={0} className="ml-4 text-textBody">
+                {formattedTimeDifference} ago
+              </span>
+            </div>
+            <button
+              onClick={handleReplyBtnClick}
+              className="flex items-start gap-x-1 font-bold text-target transition-colors hover:text-targetInactive"
+            >
+              <Reply size={18} strokeWidth={4} />
+              Reply
+            </button>
+          </div>
+          <p tabIndex={0} className="break-words leading-normal text-textBody">
+            {replyTo && (
+              <span className="mr-1 font-medium text-target">@{replyTo}</span>
+            )}
+            {content}
+          </p>
+        </div>
       </div>
+      {showAddCommentReply && (
+        <AddComment
+          replyToId={commentId}
+          replyTo={username}
+          onDone={() => setShowAddReplyComment(false)}
+        />
+      )}
     </div>
   )
 }
