86e3e0cf11406b10ea63ff4022fa78e760a960b7
Daniel <dgplays14@gmail.com>
2024-12-12 14:44:01 -0300

    feat: add Google site verification meta tag to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="NK_YRyzH-6vt7WN_fUlh27kl9Rbv60jNBhDY_H7oLb8"
+        />
+      </head>
       <body className={rubik.className}>
         <NextAuthSessionProvider>
           <ReactQueryProvider>
