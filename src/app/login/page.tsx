9602aefd4a7c602f2d6f6008f2fe9e09b5a61c67
Daniel <dgplays14@gmail.com>
2024-12-21 02:49:09 -0300

    feat: add ThemeToggleButton to login and signup pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from 'next/navigation'
 
 import AuthModal from '@/components/AuthModal'
 import Modal from '@/components/Modal'
+import ThemeToggleButton from '@/components/ToggleThemeBtn'
 
 export default async function Page() {
   const session = await getServerSession()
@@ -13,6 +14,9 @@ export default async function Page() {
 
   return (
     <Modal light>
+      <div className="fixed right-6 top-6 z-50 flex sm:hidden">
+        <ThemeToggleButton />
+      </div>
       <AuthModal type="login" useAnchorTag />
     </Modal>
   )
