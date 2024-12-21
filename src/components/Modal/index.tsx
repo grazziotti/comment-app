ed30322cf29b1541d1a3e9965f5f592b9ea640e8
Daniel <dgplays14@gmail.com>
2024-12-21 02:56:09 -0300

    feat: add non-clickable background for login and signup modals on main routes

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -16,10 +16,14 @@ export default function Modal({ children, light }: Props) {
 
   return (
     <div className="sm:fixed sm:top-0 sm:h-full sm:w-full sm:overflow-auto">
-      <Link
-        href="/"
-        className={`opacity-80" fixed bottom-0 left-0 right-0 top-0 z-10 cursor-default ${light ? 'bg-secondary' : 'bg-black'} ${!light && 'opacity-80'}`}
-      />
+      {light ? (
+        <div className="fixed bottom-0 left-0 right-0 top-0 z-10 cursor-default bg-secondary"></div>
+      ) : (
+        <Link
+          href="/"
+          className="fixed bottom-0 left-0 right-0 top-0 z-10 cursor-default bg-black opacity-80"
+        />
+      )}
       {children}
     </div>
   )
